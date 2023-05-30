c5ea9fd1a2f28ee1b3292d2b781565e053e08c39
Ibrahim Imam <ibrhmmm552@gmail.com>
2023-05-30 10:08:53 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        body: ["Inter"],
+        sora: ["Sora"],
+      },
+      colors: {
+        "bg-color": "#272727",
+        "gradient-1": "#3BF686",
+        "gradient-2": "#4CA9FF",
+        "bg-project": "#323443",
+        "bg-hover": "#4c4d5b",
+        "sec-text": "rgba(239, 237, 232, 0.6)",
+      },
+      margin: {
+        "32px": "32px",
+      },
+      height: {
+        "h-1082px": "1082px",
+        "h-338px": "338px",
+      },
+      width: {
+        18: "68px",
+      },
+    },
+  },
+  plugins: [],
+};
